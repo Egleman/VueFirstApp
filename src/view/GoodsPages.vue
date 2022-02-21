a58fcebd1fcc115d396e6019ddce40f75dd18fb5
Egleman <martsnov@hotmail.com>
2022-02-21 15:54:56 +0300

    lesson04

--- src/view/GoodsPages.vue
+++ src/view/GoodsPages.vue
@@ -71,5 +71,47 @@ export default {
     NavBarComponent,
     ProductCart,
   },
+  data() {
+    return {
+      arr: [
+        {
+          id: 0,
+          img: "coffee-1.jpg",
+          name: "Solimo Coffee Beans 2kg",
+          price: "10.73$",
+        },
+        {
+          id: 1,
+          img: "coffee-1.jpg",
+          name: "Presto Coffee Beans 1kg",
+          price: "15.99$",
+        },
+        {
+          id: 2,
+          img: "coffee-1.jpg",
+          name: "AROMISTICO Coffee 1kg",
+          price: "6.99$",
+        },
+        {
+          id: 3,
+          img: "coffee-1.jpg",
+          name: "Presto Coffee Beans 1kg",
+          price: "15.99$",
+        },
+        {
+          id: 4,
+          img: "coffee-1.jpg",
+          name: "Solimo Coffee Beans 2kg",
+          price: "10.73$",
+        },
+        {
+          id: 5,
+          img: "coffee-1.jpg",
+          name: "AROMISTICO Coffee 1kg",
+          price: "6.99$",
+        },
+      ],
+    };
+  },
 };
 </script>
--- src/view/HeroView.vue
+++ src/view/HeroView.vue
@@ -79,5 +79,29 @@ export default {
     NavBarComponent,
     ProductCart,
   },
+  data() {
+    return {
+      arr: [
+        {
+          id: 0,
+          img: "coffee-1.jpg",
+          name: "Solimo Coffee Beans 2kg",
+          price: "10.73$",
+        },
+        {
+          id: 1,
+          img: "coffee-1.jpg",
+          name: "Presto Coffee Beans 1kg",
+          price: "15.99$",
+        },
+        {
+          id: 2,
+          img: "coffee-1.jpg",
+          name: "AROMISTICO Coffee 1kg",
+          price: "6.99$",
+        },
+      ],
+    };
+  },
 };
 </script>
