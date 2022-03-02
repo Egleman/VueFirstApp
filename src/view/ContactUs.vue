15e07023425e4231930af5cc3332b1eb55a6f3a9
Egleman <martsnov@hotmail.com>
2022-03-02 20:54:45 +0300

    vuelidate

--- src/view/ContactUs.vue
+++ src/view/ContactUs.vue
@@ -21,9 +21,9 @@
               alt="Beans logo"
             />
 
-            <form action="#" class="mt-5" @submit.prevent="logger">
+            <form action="#" class="mt-5" @submit.prevent="submit">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
@@ -34,13 +34,16 @@
                     type="text"
                     class="form-control"
                     id="name-input"
-                    v-model="form.name"
+                    v-model="v$.name.$model"
                   />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">
+                    {{error.$message}}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
@@ -51,13 +54,16 @@
                     type="email"
                     class="form-control"
                     id="email-input"
-                    v-model="form.email"
+                    v-model="v$.email.$model"
                   />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">
+                    {{error.$message}}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
@@ -65,13 +71,13 @@
                     type="tel"
                     class="form-control"
                     id="phone-input"
-                    v-model="form.phone"
+                    v-model="v$.phone.$model"
                   />
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -84,9 +90,23 @@
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">
+                    {{error.$message}}
+                  </span>
                 </div>
+              </div>
+              <div class="form-group row">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
+                  <input type="checkbox" v-model="v$.checkbox.$model">
+                </div>
+                <div class="col col-12 col-sm-9">
+                  <span>Согласен с договором оферты</span>
+                </div>
+                <span v-for="error in v$.checkbox.$errors" :key="error.$uid" style="margin: 0 auto">
+                    {{error.$message}}
+                  </span>
               </div>
 
               <div class="row">
@@ -105,23 +125,54 @@
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
 
+import useVuelidate from '@vuelidate/core';
+import { required, email, maxLength } from '@vuelidate/validators';
+import { helpers } from '@vuelidate/validators';
+import { minLength } from '@/validators/minLength';
+import { checked } from '@/validators/checked';
+
 export default {
   components: {
     NavBarComponent,
   },
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  validations () {
+    return {
+      name: { required },
+      email: { required, email },
+      phone: { },
+      message: { 
+        required, 
+        maxLength: maxLength(20), 
+        minLength: helpers.withMessage('this value min 5', minLength)
+      },
+      checkbox: { 
+        required,
+        checked: helpers.withMessage('Value is required', checked)
+      }
+    }
+  },
   data() {
     return {
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
-      },
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+      checkbox: true
     };
   },
   methods: {
-    logger() {
-      console.log(this.form);
+    async submit() {
+      const isFormCorrect = await this.v$.$validate()
+      if (!isFormCorrect) return;
+      console.log({
+        name: this.name,
+        email: this.email,
+        phone: this.phone,
+        message: this.message,
+      })
     },
   },
 };
